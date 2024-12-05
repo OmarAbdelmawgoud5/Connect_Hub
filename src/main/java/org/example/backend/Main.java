343c995b99da2b8f47a2de252fb56c3ab1bfed63
Yousef nabil <yousefelhezawy75@gmail.com>
2024-12-05 09:44:32 +0200

    solid1

---
+++ src/main/java/org/example/backend/Main.java
@@ -0,0 +1,35 @@
+package org.example.Backend;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        LocalDate c = LocalDate.now();
+          var x=new User("yousef","yousefe;hezawy75",c,"D:\\Me\\vv.jpg", "D:\\Me\\vv.jpg","2611210y","online","153");
+        new Profile(x);
+        var y=new User("Omar","yousefe;hezawy75",c,"D:\\Me\\vv.jpg", "D:\\Me\\vv.jpg","2611210y","online","153");
+        var z=new User("eyad","yousefe;hezawy75",c,"D:\\Me\\vv.jpg", "D:\\Me\\vv.jpg","2611210y","online","153");
+
+        Map<User, Integer> map = new HashMap<>();
+
+        map.put(x, 30);
+        map.put(y, 6);
+
+        Map<User,Map> m=new HashMap<>();
+        m.put(z,map);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File("output.json"), m);
+
+    }
+}
---
+++ src/main/java/org/example/backend/Posts.java
@@ -0,0 +1,12 @@
+package org.example.Backend;
+
+public class Posts {
+    String Content;
+    Posts(String Content) {
+        this.Content = Content;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+}
