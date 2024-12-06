7a36224d6be19adb6e3fab67c3404a0ce4bbc9f8
Omar Abdelmawgoud <Omar.Abdelmawgoud10@gmail.com>
2024-12-06 18:55:40 +0200

    added the friend suggest functionallity

--- src/main/java/org/example/backend/UserJson.java
+++ src/main/java/org/example/backend/UserJson.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -56,5 +57,19 @@ public class UserJson {
         System.out.println(rootNode.get(id).toString());
         User user = objectMapper.readValue(rootNode.get(id).toString(), User.class);
         return  user;
+    }
+    Map<String, User> getmap() throws IOException {
+        Map<String, User> mp = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        Iterator<Map.Entry<String, JsonNode>> fields = db.rootNode.fields();
+        while (fields.hasNext()) {
+            Map.Entry<String, JsonNode> entry = fields.next();
+            JsonNode userNode = entry.getValue();
+            User user = objectMapper.readValue(userNode.toString(), User.class);
+            mp.put(entry.getKey(), user);
+        }
+        return mp;
     }
 }
